a38b3045ca2c6391b09c39519d1b8eddef8f7edc
Stella <stellajak@gmail.com>
2024-11-30 15:20:28 +0100

    scroll to top button

--- script.js
+++ script.js
@@ -117,5 +117,18 @@ document.addEventListener("DOMContentLoaded", function() {
         modalBackground.style.opacity = "0";
     });
 
-    console.log(profilePic);
+    const backToTop = document.getElementById('back-to-top');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        backToTop.style.display = 'block';
+    } else {
+        backToTop.style.display = 'none';
+    }
+});
+
+backToTop.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
 });
